3288a3b61e0d846d4c9db8e116cb704c9586add9
youra2004 <youra0305@gmail.com>
2022-03-09 14:51:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from "./App.module.css";
+import { useEffect, useState } from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import { red } from "@mui/material/colors";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [isComments, setIsComments] = useState(false);
+  const [idPost, setIdPost] = useState();
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getDatePosts();
+    getDateUser();
+    getDateComments();
+  }, []);
+
+  const getDatePosts = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+    setPosts(await res.json());
+  };
+
+  const getDateComments = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments");
+
+    setComments(await res.json());
+  };
+
+  const getDateUser = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    setUsers(await res.json());
+  };
+
+  const clickHanlder = (e) => {
+    setIsComments(true);
+    setIdPost(e.target.id);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {posts.map((post) => {
+        return users.map((user) => {
+          if (post.userId === user.id) {
+            return (
+              <Card
+                sx={{ maxWidth: 345 }}
+                key={post.id}
+                className={classes.card}
+              >
+                <CardHeader
+                  avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                      {user.name.charAt(0)}
+                    </Avatar>
+                  }
+                  title={user.name}
+                  subheader={post.title}
+                />
+                <CardContent>
+                  <Typography>{post.body}</Typography>
+                </CardContent>
+                {isComments && (
+                  <CardContent>
+                    <Typography>
+                      {comments.map((comment) => {
+                        if (
+                          idPost === comment.postId.toString() &&
+                          post.id === comment.postId
+                        ) {
+                          return `${comment.body}.`;
+                        }
+                      })}
+                    </Typography>
+                  </CardContent>
+                )}
+                <CardActions>
+                  <Button onClick={clickHanlder} id={post.id} size="small">
+                    Comment
+                  </Button>
+                </CardActions>
+              </Card>
+            );
+          }
+        });
+      })}
     </div>
   );
 }
